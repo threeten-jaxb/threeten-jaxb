a21fd187ae076b291e3d4d74dd3c5988f3314dec
Mikhail Sokolov <mikhail.g.sokolov@gmail.com>
2015-03-10 20:53:26 +0600

    Removed meaningless package-info file

--- src/main/java/com/migesok/jaxb/adapter/javatime/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * TODO: add package description
- * TODO: add adapters for types: Month, DayOfWeek, Year, YearMonth, MonthDay, ZoneOffset, ZoneId
- */
-package com.migesok.jaxb.adapter.javatime;
