f08d5d993432995ca48235d0d3d9563568ed51d5
Martin Kröning <m.kroening@hotmail.de>
2019-03-15 15:23:13 +0100

    Reformat root project file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,30 @@ subprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
     apply(plugin = "signing")
+    
     group = "io.github.threeten-jaxb"
     version = "1.1.4-SNAPSHOT"
+    
     repositories {
         mavenCentral()
     }
+    
     configure<JavaPluginConvention> {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    
     task<Jar>("sourcesJar") {
         from(project.the<SourceSetContainer>()["main"].allJava)
         archiveClassifier.set("sources")
     }
+    
     task<Jar>("javadocJar") {
         dependsOn(tasks.named<Javadoc>("javadoc"))
         from(tasks.named<Javadoc>("javadoc").get().destinationDir)
         archiveClassifier.set("javadoc")
     }
+    
     tasks.withType<Jar> {
         manifest {
             attributes(
@@ -28,14 +34,17 @@ subprojects {
             )
         }
     }
+    
     tasks.withType<Test> {
         useJUnitPlatform()
     }
+    
     dependencies {
         "testImplementation"(platform("org.junit:junit-bom:5.4.0"))
         "testImplementation"("org.junit.jupiter:junit-jupiter-api")
         "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine")
     }
+    
     afterEvaluate {
         configure<PublishingExtension> {
             publications {
@@ -43,16 +52,19 @@ subprojects {
                     from(components["java"])
                     artifact(tasks["sourcesJar"])
                     artifact(tasks["javadocJar"])
+                    
                     pom {
                         name.set(project.name)
                         description.set(project.description)
                         url.set("https://github.com/threeten-jaxb/threeten-jaxb")
+                        
                         licenses {
                             license {
                                 name.set("The Apache License, Version 2.0")
                                 url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                             }
                         }
+                        
                         developers {
                             developer {
                                 id.set("mwkroening")
@@ -65,6 +77,7 @@ subprojects {
                                 url.set("https://github.com/migesok")
                             }
                         }
+                        
                         scm {
                             connection.set("scm:git:git://github.com/threeten-jaxb/threeten-jaxb.git")
                             developerConnection.set("scm:git:ssh://github.com/threeten-jaxb/threeten-jaxb.git")
@@ -73,11 +86,13 @@ subprojects {
                     }
                 }
             }
+            
             repositories {
                 maven {
                     val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
                     val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
                     url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                    
                     credentials {
                         val ossrhUsername: String? by project
                         username = ossrhUsername
@@ -87,6 +102,7 @@ subprojects {
                 }
             }
         }
+        
         configure<SigningExtension> {
             sign(the<PublishingExtension>().publications["mavenJava"])
         }
