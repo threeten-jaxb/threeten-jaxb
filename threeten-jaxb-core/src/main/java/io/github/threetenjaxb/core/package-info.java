8b9f028647840a240701b2c68cd4d1fd9ab9c993
Martin Kröning <m.kroening@hotmail.de>
2019-03-26 19:14:58 +0100

    Provide XJC binding examples Resolves #7

---
+++ threeten-jaxb-core/src/main/java/io/github/threetenjaxb/core/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * XmlAdapters for the {@link java.time} API.
+ *
+ * <p>These classes implement {@link javax.xml.bind.annotation.adapters.XmlAdapter} and can be used with the {@link javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter} annotation.
+ *
+ * <p><b>XJC Bindings Example</b>
+ * <pre>{@code    <bindings xmlns="http://java.sun.com/xml/ns/jaxb" version="2.1"
+ *             xmlns:xjc="http://java.sun.com/xml/ns/jaxb/xjc">
+ *       <globalBindings>
+ *           <xjc:javaType name="java.time.LocalDate" xmlType="xs:date" adapter="io.github.threetenjaxb.core.LocalDateXmlAdapter" />
+ *           <xjc:javaType name="java.time.LocalDateTime" xmlType="xs:dateTime" adapter="io.github.threetenjaxb.core.LocalDateTimeXmlAdapter" />
+ *           <xjc:javaType name="java.time.YearMonth" xmlType="xs:gYearMonth" adapter="io.github.threetenjaxb.core.YearMonthXmlAdapter" />
+ *           <xjc:javaType name="java.time.Duration" xmlType="xs:duration" adapter="io.github.threetenjaxb.core.DurationXmlAdapter" />
+ *       </globalBindings>
+ *   </bindings>  }</pre>
+ *
+ * <p>Note: If you need support for <a href="https://www.w3.org/TR/xmlschema-2/#dateTime-timezones">XML Schema dateTime-timezones</a>, use {@link java.time.OffsetDateTime} instead of {@link java.time.LocalDateTime}.
+ */
+package io.github.threetenjaxb.core;
