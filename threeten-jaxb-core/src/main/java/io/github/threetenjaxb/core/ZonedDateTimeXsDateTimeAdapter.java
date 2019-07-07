3025320adb9a60fd619e3f485a7c24288dafac91
Florian Brunner <fbrunnerreg@gmx.ch>
2019-07-07 21:35:53 +0200

    Issue #16 ZonedDateTimeXmlAdapter - warning added

--- threeten-jaxb-core/src/main/java/io/github/threetenjaxb/core/ZonedDateTimeXsDateTimeAdapter.java
+++ threeten-jaxb-core/src/main/java/io/github/threetenjaxb/core/ZonedDateTimeXsDateTimeAdapter.java
@@ -6,7 +6,12 @@ import java.time.format.DateTimeFormatter;
 /**
  * {@code XmlAdapter} mapping JSR-310 {@code ZonedDateTime} to a xs:dateTime conform string.
  * <p>
+ * <strong>Attention</strong>: Please note that the {@link ZonedDateTime#getZone()} won't be marshalled as there is no
+ * representation for zoneId in xs:dateTime. Only the {@link ZonedDateTime#getOffset()} will be marshalled, if there is any.
+ * </p>
+ * <p>
  * String format details: {@link java.time.format.DateTimeFormatter#ISO_OFFSET_DATE_TIME}
+ * </p>
  *
  * @see javax.xml.bind.annotation.adapters.XmlAdapter
  * @see java.time.ZonedDateTime
